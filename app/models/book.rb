145d3a9893ca12f3b37c9b777de85e70dcc65916
Autumn J <autumnkangas@gmail.com>
2018-02-14 01:35:00 -0600

    added slugify method

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,13 @@ class Book < ActiveRecord::Base
   has_many :book_genres
   has_many :genres, through: :book_genres
 
+  def slug
+    name_array = title.downcase.split(" ")
+    name_array.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    Book.all.detect {|user| user.slug == slug }
+  end
+
 end
