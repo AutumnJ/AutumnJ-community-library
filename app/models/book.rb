a83524e023ccf5cd781a5702cd730e865bc9e7c5
Autumn J <autumnkangas@gmail.com>
2018-02-14 00:48:54 -0600

    update to relationships in book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ActiveRecord::Base
 
   belongs_to :user
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+  has_many :book_genres
+  has_many :genres, through: :book_genres
 
 end
