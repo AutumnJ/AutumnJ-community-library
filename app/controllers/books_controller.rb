7fdac444c5f5761979ee1ac4e3fe381cd3916818
Autumn J <autumnkangas@gmail.com>
2018-02-14 13:25:51 -0600

    build out methods to borrow and return books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,27 +18,10 @@ class BooksController < ApplicationController
     end       
   end
 
-  post '/bookshelf' do #need some management for duplicate entries
-    @book = Book.create(params[:book])
-      if !params[:genre][:name].empty?
-        @book.genres << Genre.create(name: params[:genre][:name])
-      end
-      if !params[:author][:name].empty?
-        @book.authors << Author.create(name: params[:author][:name])
-      end
-      @book.genre_ids = params[:genres]
-      @book.author_ids = params[:authors]
-      @book.user = current_user
-      @book.save
-
-    #redirect to '/bookshelf/#{@book.slug}'
-    redirect to '/bookshelf'
-  end
-
   get '/bookshelf/:slug' do
     if logged_in?
       @book = Book.find_by_slug(params[:slug])     
-      if logged_in? && @book.user == current_user
+      if @book.user == current_user
         erb :'/books/show_book'
       else
         redirect '/bookshelf'
@@ -48,47 +31,98 @@ class BooksController < ApplicationController
     end 
   end
 
+  post '/bookshelf' do
+    if !Book.find_by(title: params[:book][:title])
+      @book = Book.create(params[:book])
+        if !params[:genre][:name].empty?
+          genre = Genre.create(name: params[:genre][:name])
+          params[:genres] << genre.id
+        end
+        if !params[:author][:name].empty?
+          author = Author.create(name: params[:author][:name])
+          params[:authors] << author.id
+        end
 
+        @book.genre_ids = params[:genres]
+        @book.author_ids = params[:authors]
+        @book.user = current_user
+        @book.save
 
-  get '/bookshelf/:id/borrow' do
-    #find book by id
-    #verify that current user is not book owner & book is available
-    #show book info
-    #borrow button posts to below 
-    erb :'/books/borrow_book'
+      #redirect to '/bookshelf/#{@book.slug}' #not sure why this isn't working
+      redirect to '/bookshelf'
+    else #flash message 
+      redirect to '/bookshelf/new'
+    end
   end
 
-  post '/bookshelf/:id/borrow' do
-    #updates borrower, updates status
-    #redirect to main bookshelf
+  get '/bookshelf/:slug/borrow' do
+    if logged_in?
+      @book = Book.find_by_slug(params[:slug])
+      if @book.user != current_user && @book.status == "available"
+        erb :'/books/borrow_book'
+      else
+        redirect '/bookshelf'
+      end 
+    else
+      redirect '/login'
+    end       
   end
 
-  get '/bookshelf/:id/return' do
-    #find book by id
-    #verify current user id == borrower id, book status is borrowed
-    #show book info
-    #return button posts to below
-    erb :'/books/return_book'
+  post '/bookshelf/:slug/borrow' do
+    if logged_in?
+      @book = Book.find_by_slug(params[:slug])
+      @book.status = "borrowed"
+      @book.borrower = current_user.id
+      @book.save
+
+      redirect '/bookshelf'
+    else
+      redirect '/login'
+    end    
   end
 
-  post '/bookshelf/:id/return' do
-    #update borrower id to nil, book status to nil/available?
-    #redirect to main bookshelf
+  get '/bookshelf/:slug/return' do
+    if logged_in?
+      @book = Book.find_by_slug(params[:slug])
+      if @book.borrower == current_user.id && @book.status == "borrowed"
+        erb :'/books/return_book'
+      else
+        redirect '/bookshelf'
+      end 
+    else
+      redirect '/login'
+    end       
   end
 
-  get '/bookshelf/:id/edit' do
-    #find book by id
-    #verify that current user is owner of book
+  post '/bookshelf/:slug/return' do
+    if logged_in?
+      @book = Book.find_by_slug(params[:slug])
+      @book.status = "available"
+      @book.borrower = nil
+      @book.save
+
+      redirect '/bookshelf'
+    else
+      redirect '/login'
+    end    
+  end
+
+  get '/bookshelf/:slug/edit' do
+    if logged_in?
+      @book = Book.find_by_slug(params[:slug])
     #create form for user to edit book info 
     #allow user to make book available for borrowing
+    #add link to edit on show page
     erb :'/books/edit_book'
+    else
+      redirect '/login'
   end
 
-  post '/bookshelf/:id' do
+  post '/bookshelf/:slug' do
     #make book updates 
   end
 
-  get '/bookshelf/:id/delete' do
+  get '/bookshelf/:slug/delete' do
 
   end
   #for book edit logic, (outside of borrow) if book.user_id == current_user.id
