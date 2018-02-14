150973224b14d89e3d12fe69eff8d2801099e134
Autumn J <autumnkangas@gmail.com>
2018-02-13 22:55:25 -0600

    mapping out tables

---
+++ db/migrate/20180214044606_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.integer :year_published
+      t.string :language
+      t.string :status
+      t.integer :borrower #will store foreign_key id of user that is borrower
+    end
+  end
+end
