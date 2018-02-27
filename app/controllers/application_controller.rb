d0d4ae4ef0b8f7d1cf1753dc6966210564cbf3fa
Autumn J <autumnkangas@gmail.com>
2018-02-27 11:18:57 -0600

    tighten security on patch requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 require './config/environment'
+require 'sinatra/base'
+require 'sinatra/flash'
 
 class ApplicationController < Sinatra::Base
+
+  register Sinatra::Flash
 
   configure do
     set :public_folder, 'public'
@@ -26,6 +30,15 @@ class ApplicationController < Sinatra::Base
     def current_user
       User.find(session[:user_id])
     end
+
+    def current_book
+      current_user.books.all.find_by_slug(params[:slug])
+    end
+
+    def found_book
+      Book.find_by_slug(params[:slug])
+    end
+
   end
 
 end
