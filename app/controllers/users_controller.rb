b64968604b533e355278afb02db1617ad0230086
Autumn J <autumnkangas@gmail.com>
2018-02-26 18:08:28 -0600

    add flash message about email validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,35 @@
+require 'sinatra/base'
+require 'sinatra/flash'
+
 class UsersController < ApplicationController  
+
+  register Sinatra::Flash
 
   get '/signup' do
     if !logged_in?
       erb :'users/signup'
     else
+      flash[:message] = "You're already logged in!"
       redirect to '/bookshelf'
     end
   end
 
   post "/signup" do
     @user = User.new(name: params[:name], email: params[:email], username: params[:username], password: params[:password])
-    # if @user.valid?
+    if @user.valid?
       @user.save
       redirect '/login'
-    # else 
-    #   flash[:message] = "Please enter a valid email address, along with your name, username password."
-    #   redirect '/signup'
-    # end
+      else 
+      flash[:message] = "Please enter your name, a valid email address, and a username and password for future logins."
+      redirect '/signup'
+    end
   end
 
   get '/login' do
     if !logged_in?
       erb :'users/login'
     else
+      flash[:message] = "You're already logged in!"
       redirect to '/bookshelf'
     end
   end
