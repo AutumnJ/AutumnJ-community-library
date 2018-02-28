d2fe5af2e16d54d4eeebd21b20f186c68cdb63eb
Autumn J <autumnkangas@gmail.com>
2018-02-28 10:36:54 -0600

    require unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
 
